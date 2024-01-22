9974a6943dbe9c5095819e879a13429be5a6f367
Ville Niemi <vipeni35@gmail.com>
2024-01-22 18:05:24 +0200

    jou

--- App.js
+++ App.js
@@ -1,20 +1,74 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, useState } from 'react-native';
+import { Appbar, PaperProvider, TextInput, Button } from 'react-native-paper';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <PaperProvider>
+      <Login />
+    </PaperProvider>
   );
 }
+
+
+function Login(){
+  const [formData, setFormData] = useState({username: '', password: ''})
+  const [showError, setShowError] = useState(false)
+
+  const validateAndSubmit = () => {
+    setShowError(true)
+    if(formData.username.length > 0 && formData.password.length > 8){
+      setFormData({username: '',password: ''})
+      showError(false)
+    }
+  }
+  return(
+    <>
+      <MainAppBar title="Login" />
+      <View style={styles.container}>
+        <TextInput 
+          label='Username' 
+          style={styles.inputs}
+          value={formData.username}
+          onChangeText={text => setFormData({...formData, username: text})}
+          error={formData.username.length === 0 && showError}
+         />
+        <TextInput 
+          label='Password' 
+          style={styles.inputs}
+          keyboardType='visible-password'
+          value={formData.password}
+          onChangeText={text => setFormData({...formData, password: text})} 
+          error={formData.password.length === 0 && showError}
+        />
+        <Button mode='contained' style={styles.button} onPress={validateAndSubmit}>Submit</Button>
+      </View>
+    </>
+  );
+}
+
+function MainAppBar(props) {
+  return (
+    <Appbar.Header mode="center-aligned" elevated={true}>
+      <Appbar.Content title={props.title} />
+    </Appbar.Header>
+  );
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 20, 
+  },
+  inputs: {
+    margin: 15, 
+    backgroundColor: '#fcfcfc',
+  },
+  button: {
+    backgroundColor: 'blue',
+    borderRadius: 4,
+    margin: 15, 
   },
 });
